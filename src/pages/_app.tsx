0c6a5d481c4d162c95765a65d179f3ddb71b3452
topdev <topdeveloper73@gmail.com>
2021-10-28 05:30:38 +0900

    added countdown

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,22 @@
+import { ChainId, DAppProvider } from '@usedapp/core';
 import { AppProps } from 'next/app';
 
 import '@/styles/globals.css';
 
+const config = {
+  readOnlyChainId: ChainId.Mainnet,
+  readOnlyUrls: {
+    [ChainId.Mainnet]:
+      'https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+  },
+};
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <DAppProvider config={config}>
+      <Component {...pageProps} />
+    </DAppProvider>
+  );
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
+import { formatEther } from '@ethersproject/units';
+import { useEtherBalance, useEthers } from '@usedapp/core';
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Button from '@/components/buttons/Button';
 import ButtonLink from '@/components/links/ButtonLink';
@@ -7,6 +9,36 @@ import UnstyledLink from '@/components/links/UnstyledLink';
 import Seo from '@/components/Seo';
 
 export default function HomePage() {
+  const { activateBrowserWallet, account } = useEthers();
+  const etherBalance = useEtherBalance(account);
+
+  const [time, setTime] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    const finalDate = new Date('Nov 1, 2021 10:00:00').getTime();
+
+    const intervalId = setInterval(() => {
+      const distance = finalDate - Date.now();
+      setTime({
+        days: Math.floor(distance / (1000 * 60 * 60 * 24)),
+        hours: Math.floor(
+          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        ),
+        minutes: Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
+        seconds: Math.floor((distance % (1000 * 60)) / 1000),
+      });
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
     <>
       <Seo />
@@ -45,22 +77,66 @@ export default function HomePage() {
                   height={32}
                 />
               </UnstyledLink>
-              <Button className='border-none rounded-full hover:bg-pink'>
-                Connect Wallet
+              <Button
+                className={
+                  account
+                    ? 'bg-blue-600 border-none rounded-full'
+                    : 'border-none rounded-full'
+                }
+                onClick={() => activateBrowserWallet()}
+              >
+                {account
+                  ? account.slice(0, 6) +
+                    '...' +
+                    account.slice(-4) +
+                    ' ' +
+                    '(' +
+                    etherBalance +
+                    ' ETH)'
+                  : 'CONNECT WALLET'}
               </Button>
             </div>
           </header>
           <div className='flex flex-col items-center justify-center min-h-screen text-center text-white layout'>
-            <Image
-              src='/images/YNTM.png'
-              alt='You Never Taggeed Me'
-              width={982}
-              height={204}
-            />
-
-            <ButtonLink className='mt-6' href='#bid' variant='secondary'>
-              Bid now
-            </ButtonLink>
+            <div className='text-center hero-mint'>
+              <Image
+                src='/images/YNTM.png'
+                alt='You Never Taggeed Me'
+                width={982}
+                height={204}
+              />
+              <div>
+                <ButtonLink className='mt-6' href='#bid' variant='secondary'>
+                  BID NOW
+                </ButtonLink>
+              </div>
+            </div>
+            <div className='count-down'>
+              <Image
+                src='/images/YNTM-Logo-short.png'
+                alt='YNTM'
+                width={211}
+                height={202}
+              />
+              <div className='flex items-center justify-center gap-8 p-6 mt-5 text-black border border-black rounded-xl'>
+                <div className='time-box'>
+                  <p className='time-value'>{time.days}</p>
+                  <p className='time-desc'>DAYS</p>
+                </div>
+                <div className='time-box'>
+                  <p className='time-value'>{time.hours}</p>
+                  <p className='time-desc'>HOURS</p>
+                </div>
+                <div className='time-box'>
+                  <p className='time-value'>{time.minutes}</p>
+                  <p className='time-desc'>MINUTES</p>
+                </div>
+                <div className='time-box'>
+                  <p className='time-value'>{time.seconds}</p>
+                  <p className='time-desc'>SECONDS</p>
+                </div>
+              </div>
+            </div>
           </div>
           <video
             autoPlay
@@ -69,11 +145,14 @@ export default function HomePage() {
             className='absolute top-0 -z-10'
             width='100%'
             height='100%'
+            poster='/video/intro.png'
           >
-            <source src='/video/intro.mp4' />
+            <source src='/videos/intro.mp4' />
           </video>
+          <audio autoPlay>
+            <source src='/music/background.mp3' type='audio/mpeg' />
+          </audio>
         </section>
-
         <section id='bid'>
           <div className='flex justify-center gap-8 mt-80'>
             <div>
@@ -108,46 +187,56 @@ export default function HomePage() {
                 </div>
               </div>
               <div>
-                <Button className='mt-5 rounded-full' variant='outline'>
-                  Bid now
-                </Button>
+                {/* <Button className='mt-5 rounded-full' variant='outline'>
+                  BID NOW
+                </Button> */}
               </div>
             </div>
           </div>
           <div className='flex justify-center mt-28 mb-36'>
             <div>
-              <h3 className='mb-2'>Bidding History</h3>
-              <table className='font-light text-left'>
-                <thead className='text-lg border-b'>
-                  <tr>
-                    <th className='w-48'>Price</th>
-                    <th className='w-80'>From</th>
-                    <th className='w-48'>Date</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr className='text-lg border-b bg-green-50'>
-                    <td>3.75 Ξ</td>
-                    <td>xX49238239482aw32x</td>
-                    <td>5 seconds ago</td>
-                  </tr>
-                  <tr className='text-lg border-b bg-green-50'>
-                    <td>2.75 Ξ</td>
-                    <td>xX49238239482aw32x</td>
-                    <td>10 seconds ago</td>
-                  </tr>
-                  <tr className='text-lg border-b bg-green-50'>
-                    <td>1.75 Ξ</td>
-                    <td>xX49238239482aw32x</td>
-                    <td>15 seconds ago</td>
-                  </tr>
-                  <tr className='text-lg border-b bg-green-50'>
-                    <td>0.75 Ξ</td>
-                    <td>xX49238239482aw32x</td>
-                    <td>20 seconds ago</td>
-                  </tr>
-                </tbody>
-              </table>
+              <h3 className='mb-2 ml-2'>Bidding History</h3>
+              <div className='p-2 border rounded-lg'>
+                <table className='font-light text-left'>
+                  <thead className='text-lg border-b'>
+                    <tr>
+                      <th className='w-48'>Price</th>
+                      <th className='w-80'>From</th>
+                      <th className='w-48'>Date</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {/* <tr className='text-lg border-b bg-green-50'>
+                      <td>3.75 Ξ</td>
+                      <td>xX49238239482aw32x</td>
+                      <td>5 seconds ago</td>
+                    </tr>
+                    <tr className='text-lg border-b bg-green-50'>
+                      <td>2.75 Ξ</td>
+                      <td>xX49238239482aw32x</td>
+                      <td>10 seconds ago</td>
+                    </tr>
+                    <tr className='text-lg border-b bg-green-50'>
+                      <td>1.75 Ξ</td>
+                      <td>xX49238239482aw32x</td>
+                      <td>15 seconds ago</td>
+                    </tr>
+                    <tr className='text-lg border-b bg-green-50'>
+                      <td>0.75 Ξ</td>
+                      <td>xX49238239482aw32x</td>
+                      <td>20 seconds ago</td>
+                    </tr> */}
+                    <tr>
+                      <td
+                        colSpan={3}
+                        className='p-5 text-lg text-center text-gray-700'
+                      >
+                        No history
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </section>
